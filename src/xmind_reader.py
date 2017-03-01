b499e27a1eb64167846148f3de5f710bf302be79
Toby Qin <toby.qin@live.com>
2017-03-01 23:51:36 +0800

    update.

--- src/xmind_reader.py
+++ src/xmind_reader.py
@@ -1,4 +1,7 @@
 from .datatype import *
+from xml.etree import ElementTree as  ET
+import re
+import logging
 
 
 def read_xmind(xmind_path):
@@ -6,6 +9,41 @@ def read_xmind(xmind_path):
     pass
 
 
-def read_xmind_xml(content_xml_string):
+def read_xmind_xml(content_xml_path):
     """Main function to read the content xml and return test suite data."""
-    pass
+
+    with open(content_xml_path) as f:
+        xml_content = f.read()
+
+    def get_title(node):
+        return node.find('title').text
+
+    # Remove the default namespace definition (xmlns="http://some/namespace")
+    xml_content = re.sub(r'\sxmlns="[^"]+"', '', xml_content, count=1)
+
+    xml_root = ET.fromstring(xml_content)
+    try:
+        xml_root_suite = xml_root[0][0]
+        logging.info("Found test case topic: {}".format(get_title(xml_root_suite)))
+    except:
+        logging.error('Cannot find test case topic!')
+        raise
+
+    root_suite = TestSuite()
+
+    def process_step(xml_step):
+        pass
+
+    def process_testcase(xml_testcase):
+        pass
+
+    def process_suite(xml_suite):
+        for xml_testcase in xml_suite:
+            if not get_title(xml_testcase).startswith('!'):
+                process_testcase(xml_testcase)
+
+    for xml_suite in xml_root_suite[1]:
+        if xml_suite.attrib['type'] == 'attached':
+            process_suite(xml_suite)
+
+    return root_suite
--- tests/xmind_reader_tests.py
+++ tests/xmind_reader_tests.py
@@ -2,11 +2,10 @@ from src.xmind_reader import *
 from os import path
 
 
+
 def test_read_xmind_xml():
     content_xml = path.join(path.dirname(__file__), 'xmind_content.xml')
-    with open(content_xml) as f:
-        content = f.readlines()
-        test_suite = read_xmind_xml(content)
+    test_suite = read_xmind_xml(content_xml)
 
-        assert isinstance(test_suite, TestSuite)
-        assert test_suite.name == ""
+    assert isinstance(test_suite, TestSuite)
+    assert test_suite.name == ""
