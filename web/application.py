f606f560fcf495db763d63fd454bb3028413c301
Toby Qin <toby.qin@live.com>
2017-03-11 22:23:44 +0800

    update the web app.

--- web/application.py
+++ web/application.py
@@ -1,12 +1,12 @@
 import os
 import sqlite3
 from contextlib import closing
-from datetime import datetime
 from os.path import join, exists
 
-from flask import Flask, request, send_from_directory, g, render_template
-from flask import flash
+import arrow
+from flask import Flask, request, send_from_directory, g, render_template, abort
 from werkzeug.utils import secure_filename
+
 from xmind2testlink.main import xmind_to_testlink
 
 UPLOAD_FOLDER = './uploads'
@@ -44,17 +44,35 @@ def teardown_request(exception):
 
 def insert_record(xmind_name, note=''):
     c = g.db.cursor()
-    now = str(datetime.now())
+    now = str(arrow.now())
     sql = "insert into records (name,create_on,note) VALUES (?,?,?)"
     c.execute(sql, (xmind_name, now, str(note)))
     g.db.commit()
 
 
-def get_records(limit=10):
+def get_latest_record():
+    found = list(get_records(1))
+    if found:
+        return found[0]
+
+
+def get_records(limit=5):
+    short_name_length = 20
     c = g.db.cursor()
-    sql = "select * from records order by id desc limit {}}".format(int(limit))
+    sql = "select * from records where is_deleted is not null order by id desc limit {}".format(int(limit))
     c.execute(sql)
-    return c.fetchall()
+    rows = c.fetchall()
+
+    for row in rows:
+        name, short_name, create_on, note = row[1], row[1], row[2], row[3]
+
+        # shorten the name for display
+        if len(name) > short_name_length:
+            short_name = name[:short_name_length] + '...'
+
+        # more readable time format
+        create_on = arrow.get(create_on).humanize()
+        yield short_name, name, create_on, note
 
 
 def allowed_file(filename):
@@ -62,31 +80,62 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 
-@app.route('/', methods=['GET', 'POST'])
-def index(download_xml=None):
-    if request.method == 'POST':
-        file = request.files['file']
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
+def save_file(file):
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        upload_to = join(app.config['UPLOAD_FOLDER'], filename)
+
+        if exists(upload_to):
+            filename = '{}_{}.xmind'.format(filename[:-6], arrow.now().strftime('%Y%m%d_%H%M%S'))
             upload_to = join(app.config['UPLOAD_FOLDER'], filename)
 
-            if exists(upload_to):
-                filename = '{}_{}.xmind'.format(filename[:-6], datetime.now().strftime('%Y%m%d_%H%M%S'))
-                upload_to = join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(upload_to)
+        insert_record(filename)
+        g.is_success = True
 
-            file.save(upload_to)
-            xmind_to_testlink(upload_to)
-            insert_record(filename)
-            download_xml = filename[:-5] + 'xml'
-            flash('Success!')
-        else:
-            flash("<b>{}</b> is not an xmind file!".format(file.filename))
+    elif file.filename == '':
+        g.is_success = False
+        g.error = "Please select a file!"
 
-    return render_template('index.html', download_xml=download_xml)
+    else:
+        g.is_success = False
+        g.invalid_files.append(file.filename)
 
 
-@app.route('/testlink/<filename>')
+@app.route('/', methods=['GET', 'POST'])
+def index(download_xml=None):
+    g.invalid_files = []
+    g.error = None
+
+    if request.method == 'POST':
+        files = request.files.getlist('files[]')
+
+        for file in files:
+            save_file(file)
+
+        # download the xml directly if only 1 file uploaded
+        if len(files) == 1 and getattr(g, 'is_success', False):
+            download_xml = get_latest_record()[1]
+
+        if g.invalid_files:
+            g.error = "Invalid file: {}".format(','.join(g.invalid_files))
+
+    else:
+        g.upload_form = True
+
+    return render_template('index.html', download_xml=download_xml, records=list(get_records()))
+
+
+@app.route('/<filename>/to/testlink')
 def download_file(filename):
+    full_path = join(app.config['UPLOAD_FOLDER'], filename)
+
+    if not exists(full_path):
+        abort(404)
+
+    xmind_to_testlink(full_path)
+
+    filename = filename[:-5] + 'xml'
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
 
 
