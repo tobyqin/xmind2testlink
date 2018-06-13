c7548b9ccd05987e669d4bcc27ede4571cb1a63d
Toby Qin <toby.qin@live.com>
2018-06-13 19:06:17 +0800

    integrate v1 and v2 into one module.

--- tests/xmind_parser_tests.py
+++ tests/xmind_parser_tests.py
@@ -2,8 +2,7 @@ import logging
 from json import dumps
 from os.path import join, dirname
 
-import xmind2testlink.xmind_parser as v1
-import xmind2testlink.xmind_parser_v2 as v2
+import xmind2testlink.xmind_parser as parser
 from xmind2testlink.xreader import set_logger_level
 
 set_logger_level(logging.DEBUG)
@@ -14,7 +13,7 @@ xmind_v2_file = join(xml_dir, 'Test cases by xmind v2.xmind')
 
 
 def test_parse_xmind_v1():
-    test_suite = v1.xmind_to_suite(xmind_v1_file)
+    test_suite = parser.xmind_to_suite_v1(xmind_v1_file)
     assert test_suite.name == "test case by xmind"
     assert len(test_suite.sub_suites) == 5
     assert test_suite.sub_suites[0].name == "demo suite"
@@ -22,16 +21,16 @@ def test_parse_xmind_v1():
 
 
 def test_parse_xmind_v2():
-    test_suite = v2.xmind_to_suite(xmind_v2_file)
+    test_suite = parser.xmind_to_suite_v2(xmind_v2_file)
     print(dumps(test_suite.to_dict(), indent=2))
 
 
 def test_flat_suite():
-    test_suite = v2.xmind_to_suite(xmind_v2_file)
-    out = v2.flat_suite(test_suite)
+    test_suite = parser.xmind_to_suite(xmind_v2_file)
+    out = parser.flat_suite(test_suite)
     print(dumps(out, indent=2))
 
 
 def test_xmind_to_dict():
-    d = v2.xmind_to_dict(xmind_v2_file)
+    d = parser.xmind_to_dict(xmind_v2_file)
     print(dumps(d, indent=2))
--- xmind2testlink/xmind_parser.py
+++ xmind2testlink/xmind_parser.py
@@ -4,10 +4,41 @@ Module to parse xmind file into test suite and test case objects.
 
 from .sharedparser import *
 
+cache = {}
+
+
+def _open_xmind(xmind_file):
+    if not cache:
+        cache['sheet'] = xmind_to_dict(xmind_file)
+        cache['root'] = get_default_sheet(cache['sheet'])['topic']
+
 
 def xmind_to_suite(xmind_file):
-    sheet = xmind_to_dict(xmind_file)
-    root = get_default_sheet(sheet)['topic']
+    cache.clear()
+    _open_xmind(xmind_file)
+
+    if is_v2_format(cache['root']):
+        return xmind_to_suite_v2(xmind_file)
+    else:
+        return xmind_to_suite_v1(xmind_file)
+
+
+def xmind_to_suite_v1(xmind_file):
+    def parse_suite(suite_dict):
+        suite = TestSuite()
+        suite.name = suite_dict['title']
+        suite.details = suite_dict['note']
+        suite.testcase_list = []
+        testcase_topics = suite_dict.get('topics', [])
+
+        for _ in testcase_topics:
+            t = parse_testcase(_)
+            suite.testcase_list.append(t)
+
+        return suite
+
+    _open_xmind(xmind_file)
+    root = cache['root']
 
     suite = TestSuite()
     suite.name = root['title']
@@ -19,15 +50,43 @@ def xmind_to_suite(xmind_file):
     return suite
 
 
-def parse_suite(d):
-    suite = TestSuite()
-    suite.name = d['title']
-    suite.details = d['note']
-    suite.testcase_list = []
-    testcase_topics = d.get('topics', [])
+def xmind_to_suite_v2(xmind_file):
+    def parse_testcase_list(cases_dict, parent=None):
+        if is_testcase_topic(cases_dict):
+            yield parse_testcase(cases_dict, parent)
 
-    for _ in testcase_topics:
-        t = parse_testcase(_)
-        suite.testcase_list.append(t)
+        else:
+            if not parent:
+                parent = []
+
+            parent.append(cases_dict)
+            topics = cases_dict['topics'] or []
+
+            for child in topics:
+                for _ in parse_testcase_list(child, parent):
+                    yield _
+
+    def parse_suite(suite_dict):
+        suite = TestSuite()
+        suite.name = suite_dict['title']
+        suite.details = suite_dict['note']
+        suite.testcase_list = []
+        testcase_topics = suite_dict.get('topics', [])
+
+        for node in testcase_topics:
+            for t in parse_testcase_list(node):
+                suite.testcase_list.append(t)
+
+        return suite
+
+    _open_xmind(xmind_file)
+    root = cache['root']
+
+    suite = TestSuite()
+    suite.name = root['title']
+    suite.sub_suites = []
+
+    for _ in root['topics']:
+        suite.sub_suites.append(parse_suite(_))
 
     return suite
