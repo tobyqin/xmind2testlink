388df66775222497faeef42c855c9069c35d2913
Toby Qin <toby.qin@live.com>
2018-06-21 10:56:25 +0800

    update v2 web style.

--- web/application.py
+++ web/application.py
@@ -88,7 +88,7 @@ def get_latest_record():
         return found[0]
 
 
-def get_records(limit=10):
+def get_records(limit=8):
     short_name_length = 120 if V2 else 30
     c = g.db.cursor()
     sql = "select * from records where is_deleted<>1 order by id desc limit {}".format(int(limit))
