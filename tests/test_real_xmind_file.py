1d357cbb36a570fd8f64b3ef9447a0fac04ab9ee
Toby Qin <toby.qin@live.com>
2020-05-16 11:33:30 +0800

    try to publish pypi via github actions.

--- tests/test_real_xmind_file.py
+++ tests/test_real_xmind_file.py
@@ -5,8 +5,10 @@ to debug real xmind files.
 import xmind2testlink.xmind_parser as parser
 
 xmind = r"/Users/tobyqin/Desktop/bad.xmind"
+enabled = 0
 
 
 def test_parse_xmind_file():
-    out = parser.xmind_to_suite(xmind)
-    print(out)
+    if enabled:
+        out = parser.xmind_to_suite(xmind)
+        print(out)
